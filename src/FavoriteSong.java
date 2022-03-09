c1607ca4f4fe646fb3c66c93a5e33a5276090aa2
Frank Greco <fgreco@google.com>
2022-03-09 14:53:29 -0500

    Added FavoriteSong to show overloaded constructors

---
+++ src/FavoriteSong.java
@@ -0,0 +1,50 @@
+public class FavoriteSong {
+
+    private String firstName;
+    private String lastName;
+    private String favSong;
+    private String favArtist;
+    private double priceOfSong;
+    private int songYear;
+
+    public FavoriteSong(String firstName, String lastName, String favSong, String favArtist, double priceOfSong, int songYear) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.favSong = favSong;
+        this.favArtist = favArtist;
+        this.priceOfSong = priceOfSong;
+        this.songYear = songYear;
+        System.out.println("***Inside the BIG constructor");
+    }
+
+    public FavoriteSong(String firstName, String lastName) {
+        this(firstName, lastName, "No favorite song", "No favorite artist", 1.00d, 1930);
+        System.out.println("->Inside the fname/lname constructor.");
+    }
+
+    public FavoriteSong(String firstName, String lastName, String favSong) {
+       this(firstName, lastName, favSong, "Unknown", 2.00d, 2000);
+        System.out.println("->Inside the fname/lname/fsong constructor.");
+    }
+
+    public FavoriteSong() {
+        this("no Lastname", "no Firstname");
+        System.out.println("->Inside the no argment constructor.");
+    }
+
+    public void show() {
+        System.out.println("INFO: " + firstName + ":" + lastName + ":" + favSong + ":" + favArtist + ":" + priceOfSong + ":" + songYear);
+    }
+
+    /*
+     Test this class...
+     */
+    public static void main(String[] args) {
+        FavoriteSong fs = new FavoriteSong("frank", "greco");
+        fs.show();
+
+        FavoriteSong another = new FavoriteSong();
+        another.show();
+    }
+
+}
